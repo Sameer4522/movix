7e320f82c580427b0a5644714d103ee3fdbffda0
Sameer Jain <72691195+Sameer4522@users.noreply.github.com>
2023-03-30 12:09:18 +0530

    fixed issues

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -24,7 +24,7 @@ const MovieCard = ({ data, fromSearch, mediaType }) => {
         <Img className="posterImg" src={posterUrl} />
         {!fromSearch && (
           <React.Fragment>
-            <CircleRating rating={data?.vote_average.toFixed(1)} />
+            <CircleRating rating={data.vote_average.toFixed(1)} />
             <Genres data={data?.genre_ids.slice(0, 2)} />
           </React.Fragment>
         )}
